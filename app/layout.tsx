0e7abefc71879e1cbc79028b40eaf6b1ec965b06
Abinaya-Indran <abinayaofficial28@gmail.com>
2025-03-06 16:31:54 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,15 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import icon from './icon.jpg';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio | Full Stack Developer',
+  title: 'Portfolio | Dev Abinaya',
   description: 'Portfolio showcasing my skills, projects, and experience as a full stack developer',
 };
+
 
 export default function RootLayout({
   children,
@@ -18,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href={icon.src} />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           {children}
